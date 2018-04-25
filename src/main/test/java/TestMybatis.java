d21232c237d646e9f54067b202c1cc20821ede2e
liuwei01 <liuwei01@zenmen.com>
2018-04-25 15:28:42 +0800

    1.增加了测试模块 2.增加了一条测试用例

---
+++ src/main/test/java/TestMybatis.java
@@ -0,0 +1,26 @@
+import com.chen.ssm.dao.IUserDao;
+import com.chen.ssm.entity.User;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)//表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath*:spring-mvc.xml", "classpath*:spring-mybatis.xml"})
+public class TestMybatis {
+
+    private static Logger logger = Logger.getLogger(TestMybatis.class);
+
+    @Autowired
+    private IUserDao dao;
+
+    @Test
+    public void testSelectUser() throws Exception {
+        Integer id = 1;
+        User user = dao.selectByPrimaryKey(id);
+        System.out.println(user.getEmail());
+    }
+}
