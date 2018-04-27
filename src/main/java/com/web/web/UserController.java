db42bb7395ac6dfffd59da163c91acf8f3607419
liuwei01 <liuwei01@zenmen.com>
2018-04-27 14:54:46 +0800

    1.increase interface about upload avatar and upload multi files

--- src/main/java/com/web/web/UserController.java
+++ src/main/java/com/web/web/UserController.java
@@ -5,14 +5,17 @@ import com.web.entity.dbo.UserBean;
 import com.web.entity.respo.RespFeedBackBean;
 import com.web.entity.respo.RespUserBean;
 import com.web.service.UserService;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * create by kevin
@@ -78,6 +81,51 @@ public class UserController {
                 ResponseUtils.renderJson(response, respUserBean);
             }
         }
+    }
+
+    /**
+     * upload user avatar V1.0
+     *
+     * @param file
+     * @param response
+     */
+    @RequestMapping(value = "/upload/avatar", method = RequestMethod.POST, consumes = "multipart/form-data;charset=utf-8")
+    public void doUploadFile(@RequestParam("file") MultipartFile file, HttpServletResponse response) {
+        if (!file.isEmpty()) {
+            try {
+                //这里将上传得到的文件保存至 windows d:\\temp\\file 目录,linux /Users/liuwei01/Downloads
+                FileUtils.copyInputStreamToFile(file.getInputStream(), new File("/Users/liuwei01/Pictures/",
+                        System.currentTimeMillis() + file.getOriginalFilename()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        RespUserBean respUserBean = new RespUserBean();
+        respUserBean.setSuccess("0");
+        ResponseUtils.renderJson(response, respUserBean);
+    }
+
+    /**
+     * upload multi file
+     *
+     * @param multiRequest
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/doMultiUpload", method = RequestMethod.POST, consumes = "multipart/form-data;charset=utf-8")
+    public void doUploadFileMulti(MultipartHttpServletRequest multiRequest, HttpServletResponse response) throws IOException {
+        Iterator<String> filesNames = multiRequest.getFileNames();//获得所有的文件名
+        while (filesNames.hasNext()) {
+            String fileName = filesNames.next();
+            MultipartFile file = multiRequest.getFile(fileName);
+            if (!file.isEmpty()) {
+                FileUtils.copyInputStreamToFile(file.getInputStream(), new File("/Users/liuwei01/Pictures/",
+                        System.currentTimeMillis() + file.getOriginalFilename()));
+            }
+        }
+        RespUserBean respUserBean = new RespUserBean();
+        respUserBean.setSuccess("0");
+        ResponseUtils.renderJson(response, respUserBean);
     }
 
     @RequestMapping("/search")
